5760768841f707236bca729959d7214634860d12
NVTHVN BLVK <jonathan.blacas@gmail.com>
2025-07-28 13:28:45 +0200

    Clean repo, remove build artifacts, add full .gitignore

--- Builds/MacOSX/InputFilterComponent.cpp
+++
@@ -1,7 +0,0 @@
-//
-//  InputFilterComponent.cpp
-//  MidivisiVici
-//
-//  Created by Jonathan Blacas on 18/07/2025.
-//
-
--- Builds/MacOSX/InputFilterComponent.h
+++
@@ -1,7 +0,0 @@
-//
-//  InputFilterComponent.h
-//  MidivisiVici
-//
-//  Created by Jonathan Blacas on 18/07/2025.
-//
-
